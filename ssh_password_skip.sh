0db7905221997b3316715554fac12b54783c8cd8
cook-park <cook.park@osckorea.com>
2025-06-16 17:58:14 +0900

    Wip: ansible bug fix

--- ssh_password_skip.sh
+++ ssh_password_skip.sh
@@ -1,7 +1,7 @@
 remote_password="vagrant"
 master_ip="192.168.56.101"
 worker_ip="192.168.56.102"
-ssh-keygen -f ~/.ssh/id_rsa -N ""
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
 
-SSHPASS=$remote_password sshpass -e ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@$master_ip
-SSHPASS=$remote_password sshpass -e ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@$worker_ip
+SSHPASS=$remote_password sshpass -e ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519 vagrant@$master_ip
+SSHPASS=$remote_password sshpass -e ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519 vagrant@$worker_ip
